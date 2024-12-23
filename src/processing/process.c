79ab3af6b38be611e1429cc3b3dd97b29f8a94a0
DinoMalin <juliancario@orange.fr>
2024-12-23 19:42:56 +0100

    fix: counting args without redir so that a command with redirection before the command is correctly interpreted

--- src/processing/process.c
+++ src/processing/process.c
@@ -1,9 +1,8 @@
 #include "minishell.h"
 
-Command *init_cmd(char *cmd) {
+Command *init_cmd() {
 	Command *res = ft_calloc(1, sizeof(Command));
 
-	res->cmd = ft_strdup(cmd);
 	res->av = ft_calloc(1, sizeof(char *));
 	res->in_type = r_from;
 	res->out_type = r_to;
@@ -34,17 +33,22 @@ void check_redir(Command *cmd, Node **data) {
 	}
 }
 
-void analyze_command(Command *cmd, Node **data) {
+void analyze_command(Command *cmd, Node **data, int *arg_index) {
 	if (is_redir((*data)->token)) {
 		check_redir(cmd, data);
 		return;
 	}
 
-	cmd->av = clean_strsjoin(cmd->av, ft_strdup((*data)->content));
+	if (*arg_index == 0)
+		cmd->cmd = ft_strdup((*data)->content);
+	else
+		cmd->av = clean_strsjoin(cmd->av, ft_strdup((*data)->content));
+	(*arg_index)++;
 }
 
 Command *process(Node *data) {
-	int arg_index = 0;
+	int data_index = 0;
+	int arg_index = 0; // count the args w/o redirs
 	Command *head = NULL;
 	Command *curr = NULL;
 	Command *new = NULL;
@@ -52,17 +56,19 @@ Command *process(Node *data) {
 	while (data) {
 		if (data->token == t_pipe) {
 			arg_index = 0;
-		} else if (arg_index == 0) {
-			new = init_cmd(data->content);
+			data_index = 0;
+		} else if (data_index == 0) {
+			new = init_cmd();
 			if (!head)
 				head = new;
 			else
 				curr->next = new;
 			curr = new;
-			arg_index++;
+			analyze_command(curr, &data, &arg_index);
+			data_index++;
 		} else {
-			analyze_command(curr, &data);
-			arg_index++;
+			analyze_command(curr, &data, &arg_index);
+			data_index++;
 		}
 
 		if (data)
