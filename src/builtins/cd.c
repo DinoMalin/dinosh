10b664fea0637c6ea9b3ccb82e49cbf96783e18a
DinoMalin <jcario@student.42lehavre.fr>
2025-01-10 00:41:30 +0100

    cd: handle too many args

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,10 +1,15 @@
 #include "minishell.h"
 
 void cd(Command *cmd, char **env) {
+	if (cmd->ac > 2) {
+		dprintf(2, "dinosh: too many args\n");
+		return;
+	}
+
 	if (cmd->ac < 2) {
 		char *home = ft_getenv(env, "HOME");
 		if (!home) {
-			dprintf(2, "dinosh: not enough args");
+			dprintf(2, "dinosh: not enough args\n");
 			return;
 		}
 		if (chdir(home) == -1) {
