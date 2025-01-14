3ff446c510b944de9a180b56aba19f6844e056ba
DinoMalin <jcario@student.42lehavre.fr>
2025-01-14 04:43:25 +0100

    fix: fix bug caused by readline being signal unsafe

--- src/main/main.c
+++ src/main/main.c
@@ -2,12 +2,26 @@
 
 int g_signal = 0;
 
-void sig_handler(int sig) {
-	if (sig == SIGINT) {
+bool skip_hook(bool modify, bool new) {
+	static int res = false;
+	if (modify)
+		res = new;
+	return res;
+}
+
+int rl_hook() {
+	if (g_signal == 130 && !skip_hook(false, false)) {
 		printf("\n");
 		rl_on_new_line();
 		rl_replace_line("", 0);
 		rl_redisplay();
+		skip_hook(true, true);
+	}
+	return 0;
+}
+
+void sig_handler(int sig) {
+	if (sig == SIGINT) {
 		g_signal = 130;
 	}
 }
@@ -15,6 +29,7 @@ void sig_handler(int sig) {
 int main(int ac, char **av, char **envp) {
 	(void)ac;
 	(void)av;
+	(void)envp;
 	tests_parsing(envp);
 
 	signal(SIGINT, sig_handler);
@@ -25,12 +40,14 @@ int main(int ac, char **av, char **envp) {
 		.env = copy_env(envp),
 		.exit = false,
 	};
+	rl_event_hook = &rl_hook;
 
 	do {
 		ctx.input = readline("dinosh> ");
 		if (g_signal == 130) {
 			ctx.code = 130;
 			g_signal = 0;
+			skip_hook(true, false);
 		}
 		UPDATE_CODE_VAR(ctx.code);
 
