a5b1f435a7ae432f48c586ddd4d407df43c0c377
DinoMalin <jcario@student.42lehavre.fr>
2025-01-27 21:55:04 +0100

    env: skip $?

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,10 +1,18 @@
 #include "builtins.h"
 
+#define SKIP_VAR(s)										\
+	{													\
+		char *e = ft_strchr(env[i], '=');				\
+		if (e && !ft_strncmp(env[i], s, e - env[i]))	\
+			continue;									\
+	}
+
 void env(Command *cmd, char **env) {
 	if (cmd->ac > 1)
 		BUILTIN_ERROR("env: too many args");
 
 	for (int i = 0; env[i]; i++) {
+		SKIP_VAR("?");
 		printf("%s\n", env[i]);
 	}
 }
