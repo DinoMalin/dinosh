0edb231ecd730662ad8ae4f285f00f1a6eac9b20
DinoMalin <jcario@student.42lehavre.fr>
2025-02-05 23:37:10 +0100

    builtins: fix echo

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,30 +1,20 @@
 #include "builtins.h"
 
-bool has_n_flag(Command *cmd) {
-	if (cmd->ac < 2)
-		return false;
-	if (cmd->av[1][0] != '-')
-		return false;
-
-	bool res = false;
-	for (int i = 1; cmd->av[1][i]; i++) {
-		if (cmd->av[1][i] != 'n')
-			return false;
-		res = true;
-	}
-	return res;
-}
-
 void echo(Command *cmd) {
-	int n_flag = has_n_flag(cmd);
+	bool nflag = false;
+	int i = 1;
+	while (cmd->av[i] && !ft_strncmp(cmd->av[i], "-n", 2)) {
+		nflag = true;
+		i++;
+	}
 
-	for (int i = n_flag ? 2 : 1; cmd->av[i]; i++) {
+	for (; cmd->av[i]; i++) {
 		ft_putstr_fd(cmd->av[i], 1);
 		if (i < cmd->ac - 1)
 			ft_putstr_fd(" ", 1);
 	}
 
-	if (!n_flag) {
+	if (!nflag) {
 		ft_putstr_fd("\n", 1);
 	}
 }
