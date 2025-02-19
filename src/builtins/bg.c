1f252af6649ef6a8a41f16c38e9be3e07252c036
DinoMalin <jcario@student.42lehavre.fr>
2025-02-19 03:19:02 +0100

    fixup

--- src/builtins/bg.c
+++ src/builtins/bg.c
@@ -1,21 +1,18 @@
 #include "builtins.h"
 
 void bg(Command *cmd, Context *ctx) {
-	if (cmd->ac > 1) {
+	if (cmd->ac > 1)
 		BUILTIN_ERROR("bg: didn't handle more args yet im lazy");
-	}
 
 	Job *job = ctx->jobs;
 	while (job) {
 		if (job->is_current) {
 			if (job->state == STOPPED) {
-				if (kill(job->cmd->pid, SIGCONT) < 0) {
-					BUILTIN_PERROR("bg: didn't handle more args yet im lazy");
-				} else {
-					job->state = CONTINUED;
-					print_job(job, 0);
-					job->state = RUNNING;
-				}
+				if (kill(job->cmd->pid, SIGCONT) < 0)
+					BUILTIN_PERROR("bg: failed to continue job");
+				job->state = CONTINUED;
+				print_job(job, 0);
+				job->state = RUNNING;
 		 	}
 		}
 		job = job->next;
