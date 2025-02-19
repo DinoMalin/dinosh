535b1a0de49943cbd831bee0bb57779b704e06d0
DinoMalin <jcario@student.42lehavre.fr>
2025-02-19 03:22:51 +0100

    fix: handling tstp in fg
    and handling tstp exit code

--- src/builtins/fg.c
+++ src/builtins/fg.c
@@ -1,17 +1,34 @@
 #include "builtins.h"
 
 void fg(Command *cmd, Context *ctx) {
-	if (cmd->ac > 1) {
+	if (cmd->ac > 1)
 		BUILTIN_ERROR("fg: didn't handle more args yet im lazy");
-	}
 
 	Job *job = ctx->jobs;
 	while (job) {
 		Job *next = job->next;
 		if (job->is_current) {
-			print_job(job, 0);
-			waitpid(job->cmd->pid, &cmd->exit_code, 0);
-			delete_job(ctx, job->index);
+
+			if (job->state != STOPPED)
+				print_job(job, 0);
+			else {
+				if (kill(job->cmd->pid, SIGCONT) < 0)
+					BUILTIN_PERROR("bg: failed to continue job");
+				job->state = CONTINUED;
+				print_job(job, 0);
+				job->state = RUNNING;
+			}
+
+			int status = 0;
+			waitpid(job->cmd->pid, &status, WUNTRACED);
+			if (WIFEXITED(status)) {
+				cmd->exit_code = WEXITSTATUS(status);
+				delete_job(ctx, job->index);
+			} else if (WIFSTOPPED(status)) {
+				cmd->exit_code = 148;
+				job->state = STOPPED;
+				print_job(job, 0);
+			}
 		}
 		job = next;
 	}
