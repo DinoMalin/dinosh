1a581047e4b85bfc5d2338e0acae8c766d90d419
tle-dref <thomas.ledref@gmail.com>
2025-03-12 18:39:46 +0100

    Work in progress, need to fix assign when there is mutiple operations, leaks in this case, TODO priority, boolean

--- src/expanding/expand.h
+++ src/expanding/expand.h
@@ -1,6 +1,7 @@
 #include "minishell.h"
 #include <dirent.h>
 
+#define ERROR_VALUE 2147483648
 #define SAME_ID(x, y) (x && x->id == y->id)
 #define IS_WILDCARD(x, y) (SAME_ID(x, y) && x->token == t_wildcard)
 #define INCREMENT_CONTENT(curr, index)	\
@@ -12,6 +13,15 @@
 		}								\
 	}
 
+typedef struct s_arit {
+	char *value;
+	char *baseVal;
+	bool isEnv;
+	bool isOp;
+	struct s_arit *next;
+	struct s_arit *prev;
+} t_arit;
+
 char	*ft_getenv_alloc(Env *env, char *target);
 void	parameter(Env *env, Command *cmd, Parser *el, int max);
 void	var(Env *env, Parser *el, int max);
@@ -19,3 +29,4 @@ void	arithmetic(Env *env, Parser *el);
 void	tilde(Parser *el);
 void	control_substitution(Context *ctx, Command *cmd, Parser *el, int max);
 Parser	*expand_wildcard(Parser *el, int max);
+int	ft_atoi_base(char *str);
