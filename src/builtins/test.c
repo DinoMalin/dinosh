695e8821d1c9ff5e358b6c3aa54be18b1cc9b110
DinoMalin <jcario@student.42lehavre.fr>
2025-04-15 20:07:46 +0200

    test: add number comparison to test

--- src/builtins/test.c
+++ src/builtins/test.c
@@ -1,20 +1,67 @@
 #include "builtins.h"
 
+#define ARG(n) reverse ? n+1 : n
+
 void test(Command *cmd, Context *ctx) {
 	(void)ctx;
+	bool reverse = cmd->ac > 2 && !ft_strcmp(cmd->av[1], "!");
+	bool resolved = false;
 
-	if (cmd->ac == 3 && !ft_strcmp(cmd->av[1], "-z")) {
-		cmd->exit_code = ft_strlen(cmd->av[2]) > 0;
-		return;
+	if (cmd->ac == ARG(3) && !ft_strcmp(cmd->av[ARG(1)], "-z")) {
+		cmd->exit_code = ft_strlen(cmd->av[ARG(2)]) > 0;
+		resolved = true;
 	}
-	if (cmd->ac == 4 && !ft_strcmp(cmd->av[2], "=")) {
-		cmd->exit_code = !!ft_strcmp(cmd->av[1], cmd->av[3]);
-		return;
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "=")) {
+		cmd->exit_code = !!ft_strcmp(cmd->av[ARG(1)], cmd->av[ARG(3)]);
+		resolved = true;
 	}
-	if (cmd->ac == 4 && !ft_strcmp(cmd->av[2], "!=")) {
-		cmd->exit_code = !ft_strcmp(cmd->av[1], cmd->av[3]);
-		return;
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "!=")) {
+		cmd->exit_code = !ft_strcmp(cmd->av[ARG(1)], cmd->av[ARG(3)]);
+		resolved = true;
 	}
 
-	BUILTIN_ERROR("condition expected");
+
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "-eq")) {
+		if (!is_number(cmd->av[ARG(1)]) || !is_number(cmd->av[ARG(3)]))
+			BUILTIN_ERROR("numeric argument required");
+		cmd->exit_code = !(ft_atoi(cmd->av[ARG(1)]) == ft_atoi(cmd->av[ARG(3)]));
+		resolved = true;
+	}
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "-ne")) {
+		if (!is_number(cmd->av[ARG(1)]) || !is_number(cmd->av[ARG(3)]))
+			BUILTIN_ERROR("numeric argument required");
+		cmd->exit_code = !(ft_atoi(cmd->av[ARG(1)]) != ft_atoi(cmd->av[ARG(3)]));
+		resolved = true;
+	}
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "-ge")) {
+		if (!is_number(cmd->av[ARG(1)]) || !is_number(cmd->av[ARG(3)]))
+			BUILTIN_ERROR("numeric argument required");
+		cmd->exit_code = !(ft_atoi(cmd->av[ARG(1)]) >= ft_atoi(cmd->av[ARG(3)]));
+		resolved = true;
+	}
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "-gt")) {
+		if (!is_number(cmd->av[ARG(1)]) || !is_number(cmd->av[ARG(3)]))
+			BUILTIN_ERROR("numeric argument required");
+		cmd->exit_code = !(ft_atoi(cmd->av[ARG(1)]) > ft_atoi(cmd->av[ARG(3)]));
+		resolved = true;
+	}
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "-le")) {
+		if (!is_number(cmd->av[ARG(1)]) || !is_number(cmd->av[ARG(3)]))
+			BUILTIN_ERROR("numeric argument required");
+		cmd->exit_code = !(ft_atoi(cmd->av[ARG(1)]) <= ft_atoi(cmd->av[ARG(3)]));
+		resolved = true;
+	}
+	else if (cmd->ac == ARG(4) && !ft_strcmp(cmd->av[ARG(2)], "-lt")) {
+		if (!is_number(cmd->av[ARG(1)]) || !is_number(cmd->av[ARG(3)]))
+			BUILTIN_ERROR("numeric argument required");
+		cmd->exit_code = !(ft_atoi(cmd->av[ARG(1)]) < ft_atoi(cmd->av[ARG(3)]));
+		resolved = true;
+	}
+
+	if (resolved && reverse)
+		cmd->exit_code = !cmd->exit_code;
+
+	if (!resolved) {
+		BUILTIN_ERROR("condition expected");
+	}
 }
